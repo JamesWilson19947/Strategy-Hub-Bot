7975072b3d4d3e84283ce568d835f24724d48d40
James Wilson <me@jamesphilipwilson.co.uk>
2022-05-21 10:56:07 +0100

    Added rich Presence

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,33 @@
 module.exports = {
     name: 'ready',
     once: true,
-    execute(client) {
+    async execute(client) {
+
+        const statusArray = require('../data/richPresence.json');
+
+        console.log(statusArray);
+
+        async function pickPresence(){
+            const option = Math.floor(Math.random() * statusArray.length);
+
+            try {
+                await client.user.setPresence({
+                    activities: [
+                        {
+                            name: statusArray[option].content,
+                            type: statusArray[option].type,
+                            status: statusArray[option].status
+                        },
+                    ],
+                    status: statusArray[option].status
+                });
+            } catch (error){
+                console.error(error);
+            }
+
+        }
+
+        setInterval(pickPresence, 3600 * 1000); // change every 3600 seconds (1 Hour)
         console.log(`Ready! Logged in as ${client.user.tag}`);
-        client.user.setPresence({ activities: [{ name: `a simulation where I beat you at Stellaris...`, type: `PLAYING`}]})
     },
 };
