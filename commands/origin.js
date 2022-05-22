e39c2ed613db8a822109d825f740783e0df0e10a
cryptic-boop <lowgrendav@gmail.com>
2022-05-22 18:12:03 +0200

    added-origin-command

---
+++ commands/origin.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageActionRow, MessageSelectMenu } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('origin')
+        .setDescription('A command to roll Stellaris Origin.'),
+    async execute(interaction) {
+        const row = new MessageActionRow()
+            .addComponents(
+                new MessageSelectMenu()
+                    .setCustomId('origin')
+                    .setPlaceholder('Nothing selected')
+                    .addOptions([
+                        {
+                            label: 'Balanced origin',
+                            description: 'Roll a balanced origin',
+                            value: 'balancedOrigin',
+                        },
+                        {
+                            label: 'Any Origin',
+                            description: 'Roll any origin.',
+                            value: 'origin',
+                        }
+                    ]),
+            );
+        await interaction.reply({ content: 'Select balanced or any origin', components: [row], ephemeral: true });
+    },
+};
