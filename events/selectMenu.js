efbe2b6f66e2e373808fb41189031d82ca3b1906
cryptic-boop <lowgrendav@gmail.com>
2022-05-23 22:14:11 +0200

    Origins.js should be false to see if legit and random response wasn't intended for stellaris
    This should work?

--- events/selectMenu.js
+++ events/selectMenu.js
@@ -1,35 +1,47 @@
 module.exports = {
-    name: 'interactionCreate',
-    execute(interaction) {
-        if (!interaction.isSelectMenu()) return;
+  name: 'interactionCreate',
+  execute(interaction) {
+    if (!interaction.isSelectMenu()) return;
 
-        responses = [
-            'may you fight well!',
-            'you\'re in for a ride!',
-            'may you fight for freedom!'
-        ];
+    responses = [
+      'may you fight well!',
+      'you\'re in for a ride!',
+      'may you fight for freedom!'
+    ];
 
-        const randomResponse = responses[Math.floor(Math.random() * responses.length)]
+    const randomResponse = responses[Math.floor(Math.random() * responses.length)]
 
-        if (interaction.customId === 'hoi4roll') {
-            var data = require('../data/hoi4/countries.json');
-        };
-        if (interaction.customId === 'eu4roll') {
-            var data = require('../data/eu4/countries.json');
-        };
-        if (interaction.customId === 'origin') {
-            var data = require('../data/stellaris/origins.json');
-        };
+    if (interaction.customId === 'hoi4roll') {
+      var data = require('../data/hoi4/countries.json');
+    };
+    if (interaction.customId === 'eu4roll') {
+      var data = require('../data/eu4/countries.json');
+    };
+    if (interaction.customId === 'origin') {
+      var data = require('../data/stellaris/origins.json');
+    };
 
-        selection = interaction.values[0];
-        data = data[selection];
-        try {
-            const randomData = data[Math.floor(Math.random() * data.length)];
-            interaction.reply(`${interaction.user.toString()} I give you: ${randomData}, ${randomResponse}`);
-        } catch (error) {
-            console.error(error);
-            interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-        }
-
-    },
+    selection = interaction.values[0];
+    data = data[selection];
+    try {
+      if (interaction.customId === "origins") {
+        const randomData = data[Math.floor(Math.random() * data.length)];
+        interaction.reply(
+          `${interaction.user.toString()} I give you: ${randomData}`
+        );
+      }
+      else {
+        interaction.reply(
+          `${interaction.user.toString()} I give you: ${randomData}, ${randomResponse}`
+        );
+      }
+    }
+    catch (error) {
+      console.error(error);
+      interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  },
 };
